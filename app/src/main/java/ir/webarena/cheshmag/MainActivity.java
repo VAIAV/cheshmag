ff03c2338ba75298c57380d3cbd63ee9af1b5bf4
Rouhollah Mazarei <rouhollah@gmail.com>
2016-01-29 02:04:56 +0330

    working date

---
+++ app/src/main/java/ir/webarena/cheshmag/MainActivity.java
@@ -0,0 +1,22 @@
+package ir.webarena.cheshmag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textview_txt);
+
+
+
+        CalendarTool calendarTool = new CalendarTool(2016,1,29);
+
+        textView.setText(calendarTool.getIranianDate());
+    }
+}
